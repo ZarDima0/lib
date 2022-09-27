97fe2e7908799116a3898604e1890a806635531e
Dima Fibikh <dfibikhalex@gmail.com>
2022-09-27 22:51:32 +0700

    'exception,valid'

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-declare(strict_types = 1);
-error_reporting(E_ALL);
+//declare(strict_types = 1);
+//error_reporting(E_ALL);
 require_once $_SERVER['DOCUMENT_ROOT'] . './vendor/autoload.php';
 $array = [
     [
         'loc'=> 'https://site.ru/',
         'lastmod' => '2020-12-14',
-        'priority'=> 1,
+        'priority'=> null,
         'changefreq' => 'hourly',
     ],
     [
@@ -22,8 +22,19 @@ $array = [
         'changefreq' => 'hourly',
     ]
 ];
+$url = [
 
+];
+for($i=0;$i<10;$i++) {
+    $url[] =
+        [
+            'loc'=> '/',
+            'lastmod' => '2020-12-14',
+            'priority'=> 1,
+            'changefreq' => 'hourly',
+        ];
+}
 use Zardima0\Lib\SitemapGenerator;
-$path = __DIR__ . '/upload/';
-$newSitemap = new SitemapGenerator($array,'xml',$path);
+$path = __DIR__ . '/upload/ ';
+new SitemapGenerator($url,'json ',$path);
 
--- src/SitemapGenerator.php
+++ src/SitemapGenerator.php
@@ -1,30 +1,66 @@
 <?php
 namespace Zardima0\Lib;
+
 class SitemapGenerator
 {
+    private const MAX_FILE_SIZE = 52428800;
+    private const MAX_URLS_PER_SITEMAP = 50000;
+    private const MAX_URL_COUNT = 2048;
+
+    private const ALLOWED_TYPES = ['xml','csv','json'];
+    private const ALLOWED_PRI0RITY = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,];
+    private const ALLOWED_CHANGEFREQ = ['always', 'hourly', 'daily', 'weekly', 'monthly', 'yearly', 'never',];
+    private const FILE_NAME = 'sitemap';
+    private const CHANGE_FREG = [
+        'always',
+        'hourly',
+        'daily',
+        'weekly',
+        'monthly',
+        'yearly',
+        'never',
+    ];
     private $arraySite = [];
     private $path;
     private $type;
-    const ALLOWED_TYPES = ['xml','scv','json'];
 
     public function __construct(array $arrayUrl,string $type,string $path)
     {
-        $this->arraySite = $arrayUrl;
-        $this->checkPath($path);
-        $this->checkType($type);
-        $this->generateJson();
-        $this->generateCSV();
-        $this->generateXML();
+       $start = microtime(true);
+       $this->arraySite = $arrayUrl;
+       $this->validateUrls($arrayUrl);
+       $this->checkPath(rtrim($path));
+       $this->checkType(rtrim($type));
+       $this->generate();
+       echo 'Время выполнения скрипта: ' . (microtime(true) - $start) . ' sec.';
     }
-
+    private function validateUrls(array $urls)
+    {
+        if(sizeof($urls) >self::MAX_URLS_PER_SITEMAP) {
+            throw new \Zardima0\Lib\Exception\CountUrlsException('Записать можно только 50000 ссылок,сейчас ' . sizeof($urls));
+        }
+        for ($i=0;$i<sizeof($urls);$i++) {
+            if($urls[$i]['loc'] == null && mb_strlen($urls[$i]['loc'] <= self::MAX_URL_COUNT)) {
+                throw new \Zardima0\Lib\Exception\InvalidArrayException('Длинна loc должна быть от 1 до ' . self::MAX_URL_COUNT);
+            }
+            if($urls[$i]['priority'] !== null && !in_array($urls[$i]['priority'],self::ALLOWED_PRI0RITY)) {
+                throw new \Zardima0\Lib\Exception\InvalidArrayException('Значение priority должно быть ' . implode(',',self::ALLOWED_PRI0RITY));
+            }
+            if($urls[$i]['changefreq'] !== null && !in_array($urls[$i]['changefreq'],self::ALLOWED_CHANGEFREQ)) {
+                throw new \Zardima0\Lib\Exception\InvalidArrayException('Значение changefreq должно быть вот таким ' . implode(',',self::CHANGE_FREG));
+            }
+            if($urls[$i]['lastmod'] == null) {
+                throw new \Zardima0\Lib\Exception\InvalidArrayException('Значение lastmod пустое');
+            }
+            $this->arraySite[] = $urls[$i];
+        }
+    }
     private function checkType(string $type)
     {
         if(in_array(mb_strtolower($type),self::ALLOWED_TYPES)) {
-            echo 'Значение есть';
-            return false;
+            $this->type = $type;
         }else {
-            echo 'значение нет';
-            return true;
+            throw new \Zardima0\Lib\Exception\TypeException('Неверно указан формат ' . $type  . ' ,разрешенные форматы ' . implode(',',self::ALLOWED_TYPES));
         }
     }
     private function checkPath(string $path)
@@ -35,25 +71,37 @@ class SitemapGenerator
         }
         $this->path = $path;
     }
-    public function generate()
+
+    private function generate()
     {
-        var_dump($this->path);
-        file_put_contents($this->path . 'sipe.txt','Dima');
+        switch ($this->type) {
+            case 'xml':
+                $this->generateXML();
+                break;
+            case 'csv':
+                $this->generateCSV();
+                break;
+            case 'json':
+                $this->generateJson();
+                break;
+        }
     }
+
     private function generateXML()
     {
         $writer = new \XMLWriter();
         $writer->openMemory();
+        $writer->setIndent(true);
         $writer->openURI($this->path . 'sitemap.xml');
         $writer->startDocument('1.0', 'UTF-8');
         $writer->startElement('urlset');
         $writer->writeAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-        for($i=0;$i<count($this->arraySite);$i++) {
+        for($i=0;$i<sizeof($this->arraySite);$i++) {
             $writer->startElement('url');
-            $writer->writeElement('loc',htmlspecialchars($this->arraySite[$i]['loc']));
-            $writer->writeElement('lastmod', $this->arraySite[$i]['lastmod']);
-            $writer->writeElement('changefreq',$this->arraySite[$i]['changefreq']);
-            $writer->writeElement('priority', $this->arraySite[$i]['priority']);
+            $writer->writeElement('loc', htmlspecialchars($this->arraySite[$i]['loc']));
+            $writer->writeElement('lastmod',$this->arraySite[$i]['lastmod']->format(DATE_ATOM));
+            $writer->writeElement('changefreq', $this->arraySite[$i]['changefreq']);
+            $writer->writeElement('priority',  $this->arraySite[$i]['priority']);
             $writer->endElement();
         }
         $writer->endElement();
@@ -62,20 +110,15 @@ class SitemapGenerator
     }
     private function generateCSV()
     {
-        header("Content-type: text/csv");
-        header("Content-Disposition: attachment; filename=file.csv");
-        header("Pragma: no-cache");
-        header("Expires: 0");
-        $out = fopen($this->path . 'sitemap.csv','w');
+        $out = fopen($this->path . self::FILE_NAME . '.csv','w');
         fputcsv($out,array_keys($this->arraySite[0]));
         foreach ($this->arraySite as $item) {
-            var_dump($item['loc']);
             fputcsv($out,[$item['loc'],$item['lastmod'],$item['priority'],$item['changefreq']],';');
         }
         fclose($out);
     }
     private function generateJson()
     {
-        file_put_contents( $this->path . 'sitemap.json',json_encode($this->arraySite,JSON_UNESCAPED_SLASHES));
+        file_put_contents( $this->path . self::FILE_NAME . '.json',json_encode($this->arraySite,JSON_UNESCAPED_SLASHES));
     }
 }
